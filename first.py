67a96da18b3afc793dd0b7bf8d4d1e2ddd9e77f9
hk199915 <infoharshkumar99@gmail.com>
2021-04-21 01:19:33 +0530

    List Slices

--- first.py
+++ first.py
@@ -11,3 +11,8 @@ squares=[0,1,4,9,16,25,36,49,64,81]
 print(squares[2:6])
 print(squares[3:8])
 print(squares[0:1])
+
+#string formatting
+nums=[4,5,6]
+msg="Numbers:{0} {1} {2}".format(nums[0],nums[1],nums[2])
+print(msg)
