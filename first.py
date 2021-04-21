618de9fe11db17b88bcf296b1570630fd4501557
hk199915 <infoharshkumar99@gmail.com>
2021-04-21 16:21:05 +0530

    Inheritence

--- first.py
+++ first.py
@@ -38,3 +38,19 @@ class Cat:
 
 felix=Cat("ginger",4)
 print(felix.color)
+
+#Inheritence
+class Wolf:
+    def __init__(self,name,color):
+        self.name=name
+        self.color=color
+
+    def bark(self):
+        print("Grr...")
+
+class Dog(Wolf):
+    def bark(self):
+        print("Woof")
+
+husky=Dog("Max","Grey")
+husky.bark()
