f65364d152e42b830d43954ed71bf0ac33d74955
hk199915 <infoharshkumar99@gmail.com>
2021-04-21 22:24:59 +0530

    Static Methods

--- first.py
+++ first.py
@@ -95,3 +95,19 @@ class Rectangle:
 
 square=Rectangle.new_square(5)
 print(square.calculate_area())
+
+#Static Methods
+class Pizza:
+    def __init__(self,toppings):
+        self.toppings=toppings
+
+    @staticmethod
+    def validate_topping(topping):
+        if topping=="pineapple":
+            raise ValueError("No Pineapples!")
+        else:
+            return True
+
+ingridients=["cheese","onions","spam"]
+if all(Pizza.validate_topping(i) for i in ingridients):
+    pizza=Pizza(ingridients)
