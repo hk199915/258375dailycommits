b14f07cf8f27724b32ea4ddede684c5d48b0285f
hk199915 <infoharshkumar99@gmail.com>
2021-04-23 00:01:20 +0530

    Second smallest element in a list

---
+++ Second_smallest_in_list.py
@@ -0,0 +1,9 @@
+# Python prog to illustrate the following in a list
+def find_len(list1):
+	length = len(list1)
+	list1.sort()
+	print("Second Smallest element is:", list1[1])
+
+# Driver Code
+list1=[12, 45, 2, 41, 31, 10, 8, 6, 4]
+Largest = find_len(list1)
