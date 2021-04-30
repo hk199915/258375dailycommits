afac11bf743189abdd2bc380db5bb199726bd242
hk199915 <infoharshkumar99@gmail.com>
2021-05-01 00:09:49 +0530

    Practice commit

--- Pizza_rate_calculator.py
+++ Pizza_rate_calculator.py
@@ -1,9 +1,10 @@
 slices=float(input("Enter number of slices you want:"))
 
-if(slices%2==0):
-    price=120.00*slices
-    print("Total price:"+str(price))
+if(slices % 2 == 0):
+    price = 120.00 * slices
+    print("Total price:" + str(price))
 
 else:
-    price=123.00*slices
-    print("Total price:"+str(price))
+    price = 123.00 * slices
+    print("Total price:" + str(price))
+
